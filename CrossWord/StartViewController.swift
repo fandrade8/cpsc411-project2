55cbbc1c2d8b6d56b544d35b3b4183092e60cf2d
fandrade8 <f.andrade.8.10@gmail.com>
2020-05-20 08:43:24 -0700

    Add files via upload

---
+++ CrossWord/StartViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StartViewController.swift
+//  CrossWord
+//
+//  Created by CSUFTitan on 4/24/20.
+//  Copyright © 2020 CSUFTitan. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    
+    @IBOutlet var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
---
+++ CrossWord/ViewController.swift
@@ -0,0 +1,114 @@
+//
+//  ViewController.swift
+//  CrossWord
+//
+//  Created by CSUFTitan on 4/24/20.
+//  Copyright © 2020 CSUFTitan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+        @IBOutlet var button0A: UIButton!
+        @IBOutlet var button0B: UIButton!
+        @IBOutlet var button0C: UIButton!
+        @IBOutlet var button0D: UIButton!
+        @IBOutlet var button0E: UIButton!
+        @IBOutlet var button0F: UIButton!
+        @IBOutlet var button0G: UIButton!
+        @IBOutlet var button0H: UIButton!
+        @IBOutlet var button0I: UIButton!
+       
+        @IBOutlet var button1A: UIButton!
+        @IBOutlet var button1B: UIButton!
+        @IBOutlet var button1C: UIButton!
+        @IBOutlet var button1D: UIButton!
+        @IBOutlet var button1E: UIButton!
+        @IBOutlet var button1F: UIButton!
+        @IBOutlet var button1G: UIButton!
+        @IBOutlet var button1H: UIButton!
+        @IBOutlet var button1I: UIButton!
+       
+        @IBOutlet var button2A: UIButton!
+        @IBOutlet var button2B: UIButton!
+        @IBOutlet var button2C: UIButton!
+        @IBOutlet var button2D: UIButton!
+        @IBOutlet var button2E: UIButton!
+        @IBOutlet var button2F: UIButton!
+        @IBOutlet var button2G: UIButton!
+        @IBOutlet var button2H: UIButton!
+        @IBOutlet var button2I: UIButton!
+       
+        @IBOutlet var button3A: UIButton!
+        @IBOutlet var button3B: UIButton!
+        @IBOutlet var button3C: UIButton!
+        @IBOutlet var button3D: UIButton!
+        @IBOutlet var button3E: UIButton!
+        @IBOutlet var button3F: UIButton!
+        @IBOutlet var button3G: UIButton!
+        @IBOutlet var button3H: UIButton!
+        @IBOutlet var button3I: UIButton!
+       
+        @IBOutlet var button4A: UIButton!
+        @IBOutlet var button4B: UIButton!
+        @IBOutlet var button4C: UIButton!
+        @IBOutlet var button4D: UIButton!
+        @IBOutlet var button4E: UIButton!
+        @IBOutlet var button4F: UIButton!
+        @IBOutlet var button4G: UIButton!
+        @IBOutlet var button4H: UIButton!
+        @IBOutlet var button4I: UIButton!
+       
+        @IBOutlet var button5A: UIButton!
+        @IBOutlet var button5B: UIButton!
+        @IBOutlet var button5C: UIButton!
+        @IBOutlet var button5D: UIButton!
+        @IBOutlet var button5E: UIButton!
+        @IBOutlet var button5F: UIButton!
+        @IBOutlet var button5G: UIButton!
+        @IBOutlet var button5H: UIButton!
+        @IBOutlet var button5I: UIButton!
+       
+        @IBOutlet var button6A: UIButton!
+        @IBOutlet var button6B: UIButton!
+        @IBOutlet var button6C: UIButton!
+        @IBOutlet var button6D: UIButton!
+        @IBOutlet var button6E: UIButton!
+        @IBOutlet var button6F: UIButton!
+        @IBOutlet var button6G: UIButton!
+        @IBOutlet var button6H: UIButton!
+        @IBOutlet var button6I: UIButton!
+       
+        @IBOutlet var button7A: UIButton!
+        @IBOutlet var button7B: UIButton!
+        @IBOutlet var button7C: UIButton!
+        @IBOutlet var button7D: UIButton!
+        @IBOutlet var button7E: UIButton!
+        @IBOutlet var button7F: UIButton!
+        @IBOutlet var button7G: UIButton!
+        @IBOutlet var button7H: UIButton!
+        @IBOutlet var button7I: UIButton!
+       
+        @IBOutlet var button8A: UIButton!
+        @IBOutlet var button8B: UIButton!
+        @IBOutlet var button8C: UIButton!
+        @IBOutlet var button8D: UIButton!
+        @IBOutlet var button8E: UIButton!
+        @IBOutlet var button8F: UIButton!
+        @IBOutlet var button8G: UIButton!
+        @IBOutlet var button8H: UIButton!
+        @IBOutlet var button8I: UIButton!
+       
+        @IBOutlet var hintLabel: UILabel!
+       
+        @IBOutlet var stackHorizontal: UIStackView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
